ea68bb559bfc64be8399f2d549855dee17be71c1
Steven Miers <steven.miers@gmail.com>
2024-03-27 19:45:53 -0500

    Tweak docker stuff.

---
+++ bin/docker-handler-sh.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker run --add-host=host.docker.internal:host-gateway --platform linux/amd64 -it --entrypoint sh terracoil.lambda_handler
+
--- bin/docker-sh.sh
+++ bin/docker-sh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --add-host=host.docker.internal:host-gateway --platform linux/amd64 -it --entrypoint sh fast-api-jwt-fast_api_service
-# docker run --platform linux/arm64 -it  earbug:artist_harvester sh
+docker run --add-host=host.docker.internal:host-gateway --platform linux/amd64 -it --entrypoint sh terracoil.fast_api_service
+
